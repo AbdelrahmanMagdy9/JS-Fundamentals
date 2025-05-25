e70efe7f199c6aa863e37522b4577507b125fe02
Abdelrahman Magdy <125461338+AbdelrahmanMagdy9@users.noreply.github.com>
2025-05-25 20:16:33 +0300

    Multi-C

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,8 @@
 const { argv } = require('node:process');
 const x = parseInt(argv[2]);
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log("C is fun");
